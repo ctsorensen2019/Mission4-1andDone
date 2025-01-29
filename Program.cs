55cc23149662d7e360de89836ca6e252761c2bea
williaal <lailai26williams@gmail.com>
2025-01-29 08:58:33 -0700

    player1-2

--- Program.cs
+++ Program.cs
@@ -4,5 +4,15 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        string player1 = "";
+        string player2 = "";
+
+        Console.WriteLine("Welcome to the Tic-Tac-Toe!");
+        Console.WriteLine("Player 1 will use 'X' and Player 2 will use 'O'.\n");
+        Console.WriteLine("Type the name of Player 1: ");
+        player1 = Console.ReadLine();
+        Console.WriteLine("Type the name of Player 2: ");
+        player2 = Console.ReadLine();
+
     }
 }
