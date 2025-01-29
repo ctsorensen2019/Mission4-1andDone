a591e09129e7add98efb30e63ca95c089ab58962
benjiblas11 <153689062+benjiblas11@users.noreply.github.com>
2025-01-29 10:04:11 -0700

    Why isn't this working

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -12,6 +12,41 @@ public class TicTacToe
         string winner = "";
         return winner;
     }
+
+    public bool ValidChoice(string numberChoice, string numbersChosen)
+    {
+        bool result = true; //Default to a valid guess
+
+        if (numberChoice.Length != 1) //Make sure the user only enters one number
+        {
+            Console.WriteLine("Sorry, the guess has to be only one number. Try again.");
+            result = false;
+            //return false; // It's generally good practice to use return instead of setting variables while still testing the code so that you can see it's happening
+        }
+        else if (!Char.IsNumber(numberChoice[0])) //Make sure the user enters a letter and not something else
+        {
+            Console.WriteLine("Sorry, the guess needs to be a number. Try again.");
+
+        }
+        else if (numbersChosen.Contains(numberChoice)) //Check to see if the letter was already guessed
+        {
+            Console.WriteLine("Sorry, you already guessed that number");
+            result = false;
+        }
+        return result;
+
+    public void UpdateBoard(string numberChoice, char[] board, currentSymbol)
+    {
+
+        for (int i = 0; i < board.Length; i++)
+        {
+            if (numberChoice.Contains(board[i]))
+            {
+                board[i] = currentSymbol;
+            }
+        }
+    }
+}
 }
 //WAYS TO WIN
 // 0 1 2 
