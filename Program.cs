7291d067d8dcf1f01373cb0e8be0f9abc9d6c22b
williaal <lailai26williams@gmail.com>
2025-01-29 09:06:48 -0700

    made board game array

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ internal class Program
         string player1 = "";
         string player2 = "";
 
+        //Making an array of length 9
+        char[] board = new char[9];
+
         //Welcome Message to Players
         Console.WriteLine("Welcome to the Tic-Tac-Toe!");
         Console.WriteLine("Player 1 will use 'X' and Player 2 will use 'O'.\n");
@@ -16,7 +19,11 @@ internal class Program
         Console.WriteLine("Type the name of Player 2: ");
         player2 = Console.ReadLine();
 
-        //Creating character board array
+        //adding numbers as characters to board array
+        for (int i = 0;i < board.Length; i++)
+        {
+            board[i] = (i + 1).ToString()[0]; //Filling the board with numbers 1-9 as characters not int
+        }
 
 
 
