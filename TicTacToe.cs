0e3f8591efe9c8d4b9813a78cc8d9564a66afe9f
Christian Sorensen <ctsorensen2019@gmail.com>
2025-01-29 10:30:56 -0700

    ll

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -15,25 +15,20 @@ public class TicTacToe
 	public Boolean CheckWinner(char[] board, string player1, string player2, char currentSymbol)
 	{
 		Boolean winner = false;
-        
-        for (int i = 0; i < 3; i++)
+
         {
-            if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+            int[,] winningCombinations = {
+            { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // Rows
+            { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // Columns
+            { 0, 4, 8 }, { 2, 4, 6 } // Diagonals
+        };
+
+            foreach (var combo in winningCombinations)
             {
-                winner = true;
+                if (board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]])
+                    winner = true;
             }
-            if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
-            {
-                winner = true;
-            }
-        }
-        if (board[0] == board[1, 1] && board[1, 1] == board[8])
-        {
-            winner = true;
-        }
-        if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
-        {
-            winner = true;
+
         }
 
         return winner;
